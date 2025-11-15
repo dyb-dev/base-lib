690c976fe49308b7b26495ddd5cd103adc38504b
dyb-dev <2737993954@qq.com>
2025-11-15 14:10:06 +0800

    🌈 style: 更新文件注释格式

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,7 @@
+/*
+ * @FileDesc: eslint 配置 注意：每次配置的更改，建议重启一下编辑器，否则可能不会生效
+ */
+
 module.exports = {
     extends: ["@dyb-dev/eslint-config"]
 }
--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,10 +1,10 @@
 /*
  * @Author: dyb-dev
  * @Date: 2024-08-21 12:02:33
- * @LastEditors: dyb-dev
- * @LastEditTime: 2024-08-28 20:35:47
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2025-11-15 13:05:56
  * @FilePath: /base-lib/.prettierrc.mjs
- * @Description: prettier配置文件 注意：每次配置文件的更改，建议重启一下vscode，否则可能不会生效
+ * @Description: prettier 配置 注意：每次配置的更改，建议重启一下编辑器，否则可能不会生效
  */
 
 import prettierConfig from "@dyb-dev/prettier-config"
